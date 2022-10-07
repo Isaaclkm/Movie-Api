ba2d2cb90d21e54bbef34c52ea2edb9db2bb6909
Isaaclkm <isaacfelixmatos@gmail.com>
2022-10-06 20:06:13 -0400

    What to do

--- script.js
+++ script.js
@@ -7,11 +7,27 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm){
-    const URL = `https://omdbapi.com/?=${searchTerm}&page=1&apikey=dd42fda7`;
-    const res = await fetch(`${URL}`)
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=dd42fda7`;
+    const res = await fetch(`${URL}`);
     const data = await res.json();
     // console the data Search
-    if(data.Response == 'True') console.log(data.Search);
+    if(data.Response == 'True') displayMovieList(data.Search);
 }
 
-loadMovies('lord of the rings');
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    // console.log(searchTerm)
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm)
+    } else{
+         searchList.classList.add('hide-search-list');
+    }
+}
+function displayMovieList(movies){
+    searchList.innerHTML = "";
+    for(let idx = 0; idx < movies.length; idx++){
+        let movieListItem = document.createElement('div');
+        console.log(movieListItem);
+    }
+}
