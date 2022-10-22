3863dc02b274c96baadbfaba05e588ded101a4d8
Isaaclkm <isaacfelixmatos@gmail.com>
2022-10-21 23:26:28 -0400

    deleted comment

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
-//more comment
 
 
 // load movies from API
