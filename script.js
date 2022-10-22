f00ae23129144c36ef3de4c011295fa66aa01e7e
Isaaclkm <isaacfelixmatos@gmail.com>
2022-10-21 23:25:42 -0400

    more comments

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
+//more comment
 
 
 // load movies from API
