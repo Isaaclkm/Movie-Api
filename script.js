d8f5e099131fb229cba46524ad09e1339194c3ae
Isaaclkm <isaacfelixmatos@gmail.com>
2022-10-22 23:43:37 -0400

    took off

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
 
 
-// load movies from API more comments
+// load movies from API
 async function loadMovies(searchTerm){
     const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=dd42fda7`;
     const res = await fetch(`${URL}`);
